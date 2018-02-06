7cbf1df5569a744130b597ed53789deb5341d39a
Piotr Wiercinski <piotrwier@o2.pl>
2018-02-06 12:48:46 +0100

    UserInput UserOutput CREATED

---
+++ src/UserInput/UserInput.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const userInput = () => {
+  return(
+    <div>
+      <input type="text" />
+    </div>
+  ); 	
+};
+
+export default userInput;
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const userOutput = () => {
+  return (
+    <div>
+      <p>First Paragraph</p>  
+      <p>Second Paragraph</p>
+    </div>
+  );
+};
+
+export default userOutput;
